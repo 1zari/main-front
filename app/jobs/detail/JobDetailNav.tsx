e7ea38aada31fdca9eb3b588004edd89bc1d9bd5
sasha <sasha.park.designer@gmail.com>
2025-04-11 01:33:09 +0900

    ✨ [채용공고] JobDetailNav 컴포넌트에서 뒤로가기 버튼 클릭 이벤트 추가 및 코드 정리

--- app/jobs/detail/JobDetailNav.tsx
+++ app/jobs/detail/JobDetailNav.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { FaChevronLeft } from "react-icons/fa6"
 import SaveBtn from "../../../components/\bSaveBtn"
 
@@ -6,12 +8,10 @@ export default function JobDetailNav() {
     <>
       <nav className="bg-gray-z text-black p-4 sticky top-0 z-10">
         <ul className="w-full max-w-7xl mx-auto flex flex-wrap justify-between items-center gap-5">
-          <li>
-            <a href="#" className="font-bold">
-              <FaChevronLeft className="inline-block mr-2" />
-              뒤로가기
-            </a>
-          </li>
+            <li onClick={() => window.history.back()} className="font-bold cursor-pointer">
+            <FaChevronLeft className="inline-block mr-2" />
+            뒤로가기
+            </li>
 
           <li>
             <button className="flex items-center  gap-2">
