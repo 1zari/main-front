ca6cd8bdab7fb533728aea62a5621c99041430c7
sasha <sasha.park.designer@gmail.com>
2025-04-11 01:34:59 +0900

    ✨ [버튼] SaveBtn 컴포넌트의 구조 개선 및 마운트 상태 관리 추가

--- components/SaveBtn.tsx
+++ components/SaveBtn.tsx
@@ -1,18 +1,24 @@
 "use client"
 
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { FaRegStar, FaStar } from "react-icons/fa"
 
 export default function SaveBtn() {
   const [isSaved, setIsSaved] = useState(false)
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) return null
+
   return (
-    <>
-      <button
-        className="rounded-full p-2 text-xl"
-        onClick={() => setIsSaved(!isSaved)}
-      >
-        {isSaved ? <FaStar className="text-primary" /> : <FaRegStar />}
-      </button>
-    </>
+    <button
+      className="rounded-full p-2 text-xl"
+      onClick={() => setIsSaved(!isSaved)}
+    >
+      {isSaved ? <FaStar className="text-primary" /> : <FaRegStar />}
+    </button>
   )
 }
