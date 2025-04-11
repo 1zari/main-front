908213431cb3c7a342a93dfffa23fb9bd19f6f2f
sasha <sasha.park.designer@gmail.com>
2025-04-12 04:06:28 +0900

    ✨ [홈페이지] 새로운 컴포넌트 추가 및 레이아웃 개선: LandingReview, LandingPartnerCompany, JobCard, JobSearch, 페이지 구성 요소 업데이트

--- src/features/home/components/JobsArea.tsx
+++ src/features/home/components/JobsArea.tsx
@@ -14,22 +14,10 @@ export default function JobsArea() {
     <>
       <section className="w-full max-w-7xl mx-auto my-8 px-4 ">
         <div className="flex justify-between items-center py-6 mb-4">
-          <h2 className="text-2xl font-bold">최근에 등록된 공고</h2>
+          <h2 className="text-2xl font-semibold">최근에 등록된 공고</h2>
           <MoreButton />
         </div>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-7">
-          <JobCard />
-          <JobCard />
-          <JobCard />
-          <JobCard />
-          <JobCard />
-          <JobCard />
-          <JobCard />
-          <JobCard />
-          <JobCard />
-          <JobCard />
-          <JobCard />
-          <JobCard />
           <JobCard />
         </div>
       </section>
