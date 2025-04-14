21a1a281685bf149138f35509055f225835f1d4f
sasha <sasha.park.designer@gmail.com>
2025-04-14 16:53:02 +0900

    ✨ [공고 페이지] SelectedChips 컴포넌트 추가

--- src/app/jobs/by-field/page.tsx
+++ src/app/jobs/by-field/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import SelectedChips from "@/features/jobs/components/SelectedChips";
 import { useEffect } from "react";
 import JobCard from "../../../features/home/components/JobCard";
 import JobFilter from "../../../features/jobs/components/JobFilter";
@@ -16,6 +17,7 @@ export default function JobsByFieldPage() {
     <>
       <div className="w-full max-w-7xl mx-auto px-6">
         <JobFilter />
+        <SelectedChips />
       </div>
       <div className="bg-gray-z-light py-6">
         <section className="w-full max-w-7xl mx-auto my-8 px-4 ">
