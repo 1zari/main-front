85861d2996d88c50d811961458fb34ed86eef4bd
sasha <sasha.park.designer@gmail.com>
2025-04-18 18:11:23 +0900

    ✨ [지원자 조회] 지원자 상세 페이지 및 목록 컨테이너 추가, 데이터 모델링

--- src/features/applicants/components/ApplicantsListContainer.tsx
+++ src/features/applicants/components/ApplicantsListContainer.tsx
@@ -1,12 +1,56 @@
-import ApplicantsListItem from "@/features/applicants/components/ApplicantsListItem";
+"use client";
+
+import { Applicants } from "@/features/applicants/data/mockApplicants";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import ApplicantsListControlArea from "./ApplicantsListControlArea";
 
 export default function ApplicantsListContainer() {
+  const router = useRouter();
+  const [selectedApplicant, setSelectedApplicant] = useState(null);
+  const [showUnreadOnly, setShowUnreadOnly] = useState(false);
+  const applicants = Applicants;
+
   return (
     <>
-      <ApplicantsListItem />
-      <ApplicantsListItem />
-      <ApplicantsListItem />
-      <ApplicantsListItem />
+      <ApplicantsListControlArea
+        showUnreadOnly={showUnreadOnly}
+        setShowUnreadOnly={setShowUnreadOnly}
+      />
+      <div></div>
+      <p className="text-gray-500 mb-2">
+        총 {""}
+        <span className="text-primary">
+          {applicants.filter((applicant) => !showUnreadOnly || !applicant.isRead).length}
+        </span>
+        건의 지원서
+      </p>
+      <section>
+        <div className="border rounded-md divide-y">
+          {applicants
+            .filter((applicant) => !showUnreadOnly || !applicant.isRead)
+            .map((applicant) => (
+              <div
+                key={applicant.id}
+                className="p-4 hover:bg-gray-50 cursor-pointer flex justify-between items-center"
+                onClick={() => router.push(`/applicants/${applicant.id}`)}
+              >
+                <div>
+                  <p className="font-medium">{applicant.name}</p>
+                  <p className="">{applicant.coverLetter}</p>
+                  <p className="text-sm text-gray-500">{applicant.jobTitle}</p>
+                </div>
+                <div className="flex flex-col items-end">
+                  <div className="text-sm text-gray-400">지원일</div>
+                  <div className="text-sm text-gray-400">{applicant.date}</div>
+                  <div className={`text-sm ${applicant.isRead ? "text-gray-400" : "text-primary"}`}>
+                    {applicant.isRead ? "읽음" : "안읽음"}
+                  </div>
+                </div>
+              </div>
+            ))}
+        </div>
+      </section>
     </>
   );
 }
--- src/features/applicants/components/ApplicantsListControlArea.tsx
+++ src/features/applicants/components/ApplicantsListControlArea.tsx
@@ -1,22 +1,34 @@
-export default function ApplicantsListControlArea() {
+export default function ApplicantsListControlArea({ showUnreadOnly, setShowUnreadOnly }: Props) {
   return (
     <>
-      <div className="flex justify-between">
+      <div className="flex flex-col md:flex-row gap-3 justify-between">
         <div className="flex items-center gap-4">
-          <label className="text-sm font-medium">채용공고</label>
-          <select className="border px-3 py-2 rounded-md">
-            <option>전체</option>
+          <select className="w-full border px-3 py-2 rounded-md">
+            <option>채용공고 전체</option>
             <option>디자인 인턴</option>
             <option>프론트엔드 개발자</option>
             <option>운영 지원</option>
           </select>
         </div>
-        <div>
-          <select className="border px-3 py-2 rounded-md">
-            <option>최신순</option>
-            <option>이름순</option>
-            <option>지원일순</option>
-          </select>
+        <div className="flex justify-end gap-4">
+          <div>
+            <select className="border px-3 py-2 rounded-md">
+              <option>최신순</option>
+              <option>이름순</option>
+              <option>지원일순</option>
+            </select>
+          </div>
+          <div className="border px-3 py-2 rounded-md flex items-center h-[40px]">
+            <label className="flex items-center gap-1 text-sm">
+              <input
+                type="checkbox"
+                className="accent-primary"
+                onChange={(e) => setShowUnreadOnly(e.target.checked)}
+                checked={showUnreadOnly}
+              />
+              안읽음만 보기
+            </label>
+          </div>
         </div>
       </div>
     </>
