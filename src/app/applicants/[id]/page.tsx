93dec06c3f10b89b01e567ed574383632fe62fc5
sasha <sasha.park.designer@gmail.com>
2025-04-22 12:52:02 +0900

    ✨ [채용공고 상세] ApplicantDetailPage 컴포넌트 생성 및 ApplicantsResume, BackNavOnApplicatns 컴포넌트 추가

--- src/app/applicants/[id]/page.tsx
+++ src/app/applicants/[id]/page.tsx
@@ -1,3 +1,13 @@
+import ApplicantsResume from "@/features/applicants/components/ApplicantsResume";
+import BackNavOnApplicatns from "@/features/applicants/components/BackNavOnApplicatns";
+
 export default function ApplicantDetailPage() {
-  return <>ApplicantDetailPage</>;
+  return (
+    <>
+      <div className="px-4">
+        <BackNavOnApplicatns />
+        <ApplicantsResume />
+      </div>
+    </>
+  );
 }
