5297cd9175c2c7bb671c736bfc9814da82afe3bc
sasha <sasha.park.designer@gmail.com>
2025-04-23 13:42:11 +0900

    🐛 [홈] CompanyDashboard 컴포넌트에 '빌드테스트' 텍스트 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
     <main className="flex flex-col items-center min-h-screen">
       <WelcomeBanner />
       <div className="w-full text-center bg-gray-z">
-        <CompanyDashboard />
+        <CompanyDashboard /> 빌드테스트
       </div>
       <JobSearch />
       <div className="w-full bg-gray-z">
