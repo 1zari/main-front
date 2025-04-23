70f75dd76f48383eee5a6d0cdf57cf086b98ecf7
AN JEONG EUN <52526916+chiyo-an@users.noreply.github.com>
2025-04-23 16:03:22 +0900

    🔧lint 및 prettier 설정완

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,21 @@
 import eslintPluginPrettier from 'eslint-plugin-prettier';
+import typescriptPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
 
 export default [
   {
     files: ['**/*.{js,jsx,ts,tsx}'],
     languageOptions: {
+      parser: tsParser,
       ecmaVersion: 2021,
       sourceType: 'module',
     },
     plugins: {
       prettier: eslintPluginPrettier,
+      '@typescript-eslint': typescriptPlugin,
     },
     rules: {
+      ...typescriptPlugin.configs.recommended.rules,
       'prettier/prettier': 'error',
     },
   },
