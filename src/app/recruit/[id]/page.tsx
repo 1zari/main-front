2387b41c4bc35829a33418e3556aae840b51cd02
sasha <sasha.park.designer@gmail.com>
2025-04-25 16:17:07 +0900

    ♻️ [타입 개선] RecruitEditPage에서 params를 비동기 함수로 변경

--- src/app/recruit/[id]/page.tsx
+++ src/app/recruit/[id]/page.tsx
@@ -2,7 +2,7 @@
 
 import BackButton from "@/features/resume/components/common/BackButton";
 import RecruitForm from "../../../features/recruit/components/Form";
-const RecruitEditPage = ({ params }: { params: { id: string } }) => {
+const RecruitEditPage = async ({ params }: { params: { id: string } }) => {
   const jobPostingId = params.id;
 
   if (!jobPostingId) return <p>잘못된 접근입니다.</p>;
