95108c77089d78f4275d5b1a2e79550c5d51636d
sasha <sasha.park.designer@gmail.com>
2025-04-30 00:08:46 +0900

    ✨ [채용공고 등록] 직종 및 경력, 학력 입력 필드 수정 및 추가, 근무 시간 협의 옵션 추가

--- src/features/recruit/schemas/jobPostSchema.ts
+++ src/features/recruit/schemas/jobPostSchema.ts
@@ -2,21 +2,39 @@ import { z } from "zod";
 
 export const jobPostSchema = z.object({
   title: z.string().min(1, "공고 제목을 입력해주세요.").max(50, "50자 이내로 작성해주세요."),
-  occupation: z.string().min(1, "직종을 입력해주세요."),
-  employmentType: z.enum(["정규직", "계약직"]),
+  occupation: z.array(z.string()).min(1, "직종을 1개 이상 선택해주세요."),
+  employmentType: z
+    .union([z.enum(["정규직", "계약직"]), z.null()])
+    .refine((val) => val !== null, { message: "고용형태를 선택해주세요." }),
   numberOfRecruits: z
     .number({ invalid_type_error: "모집인원을 숫자로 입력해주세요." })
     .positive("1명 이상 입력해주세요."),
-  career: z.enum(["경력", "경력무관"]),
-  education: z.enum(["고졸", "대졸", "학력무관"]),
-  location: z.string().min(1, "근무지를 입력해주세요."),
+  career: z
+    .union([z.enum(["경력", "경력무관"]), z.null()])
+    .refine((val) => val !== null, { message: "경력여부를 선택해주세요." }),
+  education: z
+    .union([z.enum(["고졸", "대졸", "학력무관"]), z.null()])
+    .refine((val) => val !== null, { message: "학력을 선택해주세요." }),
+  location: z.string().min(1, "주소 검색을 눌러주세요."),
+  locationDetail: z.string().trim().min(1, "상세주소를 입력해주세요."),
   deadline: z.string(),
   salary: z.number({ invalid_type_error: "급여를 숫자로 입력해주세요." }),
-  workingDays: z.array(z.enum(["월", "화", "수", "목", "금", "토", "일"])),
-  workingHours: z.object({
-    start: z.string(),
-    end: z.string(),
-  }),
+  workingDays: z.array(z.enum(["월", "화", "수", "목", "금", "토", "일", "요일협의"])),
+  workingHours: z
+    .object({
+      start: z.string(),
+      end: z.string(),
+      negotiable: z.boolean().optional(), // 추가
+    })
+    .refine(
+      ({ start, end, negotiable }) => {
+        return (start && end) || negotiable;
+      },
+      {
+        message: "근무 시간을 입력하거나 시간협의를 선택해주세요.",
+        path: ["start"],
+      },
+    ),
   jobSummary: z.string().max(50),
   jobDescription: z.string(),
   agreeTerms: z.boolean().refine((v) => v === true, { message: "이용약관에 동의해야 합니다." }),
