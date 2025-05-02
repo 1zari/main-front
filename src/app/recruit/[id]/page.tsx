0d6b62ea8794660f3908ea49447a820cf6157f02
sasha <sasha.park.designer@gmail.com>
2025-05-02 15:48:03 +0900

    ✨ [채용공고 수정] 제목 수정 및 불필요한 컴포넌트 제거

--- src/app/recruit/[id]/page.tsx
+++ src/app/recruit/[id]/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import BackButton from "@/features/resume/components/common/BackButton";
 import RecruitForm from "../../../features/recruit/components/common/Form";
 const RecruitEditPage = async ({ params }: { params: { id: string } }) => {
   const jobPostingId = params.id;
@@ -9,8 +8,7 @@ const RecruitEditPage = async ({ params }: { params: { id: string } }) => {
 
   return (
     <div className="max-w-3xl px-4 py-8 mx-auto">
-      <BackButton />
-      <h1 className="text-2xl font-bold text-[#0F8C3B] mb-6">채용공고 수정</h1>
+      <h1 className="text-2xl font-bold text-[#0F8C3B] mb-6">채용공고 수정aa</h1>
       <RecruitForm mode="edit" jobPostingId={jobPostingId} />
     </div>
   );
