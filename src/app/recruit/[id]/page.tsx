1873e6380aec6147b5ecd8999660931cf1b8e9b5
sasha <sasha.park.designer@gmail.com>
2025-05-09 17:21:39 +0900

    [수정] RecruitEditPage에서 RecruitForm 컴포넌트 제거

--- src/app/recruit/[id]/page.tsx
+++ src/app/recruit/[id]/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import RecruitForm from "../../../features/recruit/components/common/Form";
 const RecruitEditPage = async ({ params }: { params: { id: string } }) => {
   const jobPostingId = params.id;
 
@@ -9,7 +8,6 @@ const RecruitEditPage = async ({ params }: { params: { id: string } }) => {
   return (
     <div className="max-w-3xl px-4 py-8 mx-auto">
       <h1 className="text-2xl font-bold text-[#0F8C3B] mb-6">채용공고 수정aa</h1>
-      <RecruitForm mode="edit" jobPostingId={jobPostingId} />
     </div>
   );
 };
