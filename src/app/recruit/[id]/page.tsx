bc5f79bc9e92eaee852ca836605afa566fb909e8
sasha <sasha.park.designer@gmail.com>
2025-05-10 20:25:20 +0900

    [수정] RecruitEditPage에서 JobDetailContent 컴포넌트를 추가하여 채용공고 세부정보를 표시

--- src/app/recruit/[id]/page.tsx
+++ src/app/recruit/[id]/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import JobDetailContent from "@/features/jobs/components/JobDetailContent";
+
 const RecruitEditPage = async ({ params }: { params: { id: string } }) => {
   const jobPostingId = params.id;
 
@@ -8,6 +10,7 @@ const RecruitEditPage = async ({ params }: { params: { id: string } }) => {
   return (
     <div className="max-w-3xl px-4 py-8 mx-auto">
       <h1 className="text-2xl font-bold text-[#0F8C3B] mb-6">채용공고</h1>
+      <JobDetailContent jobPostingId={jobPostingId} />
     </div>
   );
 };
