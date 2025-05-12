c8171648b62e9884bbc2621c82d12712c89e6c30
sasha <sasha.park.designer@gmail.com>
2025-05-12 11:17:47 +0900

    CompanyDashboard에서 Link 컴포넌트를 추가하여 DashboardCard를 감싸고, DashboardCard의 너비를 수정함

--- src/features/home/components/CompanyDashboard.tsx
+++ src/features/home/components/CompanyDashboard.tsx
@@ -1,6 +1,7 @@
 import { FadeInUp } from "@/components/motion/FadeInUp";
 import { Heading } from "@/components/ui/Heading";
 import DashboardCard from "@/features/home/components/DashboardCard";
+import Link from "next/link";
 
 export default function CompanyDashboard({ className }: { className?: string }) {
   const jobCount = "2";
@@ -15,8 +16,12 @@ export default function CompanyDashboard({ className }: { className?: string }) 
       </FadeInUp>
       <FadeInUp delay={0.3}>
         <div className="flex w-full justify-center gap-4 px-4 pt-4 pb-10">
-          <DashboardCard title="채용 진행중 공고" value={jobCount} unit="건" />
-          <DashboardCard title="새로운 지원자" value={applicantCount} unit="명" />
+          <Link href="/recruit">
+            <DashboardCard title="채용 진행중 공고" value={jobCount} unit="건" />
+          </Link>
+          <Link href="/applicants">
+            <DashboardCard title="새로운 지원자" value={applicantCount} unit="명" />
+          </Link>
         </div>
       </FadeInUp>
     </div>
