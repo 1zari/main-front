d219f451b29bd9956a4a706ef788f8a269f90b45
sasha <sasha.park.designer@gmail.com>
2025-05-13 15:22:18 +0900

    FindPasswordPage 및 AUTH_ROUTES의 경로 수정: 사용자 유형을 'normal'로 변경하고 경로를 일관되게 업데이트

--- src/app/(auth)/auth/[type]/find-password/page.tsx
+++ src/app/(auth)/auth/[type]/find-password/page.tsx
@@ -1,16 +1,16 @@
 "use client";
 
-import { useParams } from "next/navigation";
-import UserFindPasswordForm from "@/features/auth-user/ui/login/UserFindPasswordForm";
 import CompanyFindPasswordForm from "@/features/auth-company/ui/login/CompanyFindPasswordForm";
+import UserFindPasswordForm from "@/features/auth-user/ui/login/UserFindPasswordForm";
+import { useParams } from "next/navigation";
 
 export default function FindPasswordPage() {
   const params = useParams();
-  const type = params.type as "user" | "company";
+  const type = params.type as "normal" | "company";
 
-  if (type !== "user" && type !== "company") {
+  if (type !== "normal" && type !== "company") {
     return null; // 또는 에러 페이지 컴포넌트
   }
 
-  return <>{type === "user" ? <UserFindPasswordForm /> : <CompanyFindPasswordForm />}</>;
+  return <>{type === "normal" ? <UserFindPasswordForm /> : <CompanyFindPasswordForm />}</>;
 }
