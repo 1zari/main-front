244f780056ed462a5dddf807ea5f57429f94f0a9
sasha <sasha.park.designer@gmail.com>
2025-05-14 13:33:26 +0900

    Refactor code structure for improved readability and maintainability

--- src/app/jobs/[id]/page.tsx
+++ src/app/jobs/[id]/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useSession } from "next-auth/react";
 import { useParams } from "next/navigation";
 import JobDetailContent from "../../../features/jobs/components/JobDetailContent";
 import JobDetailNav from "../../../features/jobs/components/JobDetailNav";
@@ -6,11 +7,13 @@ import JobDetailNav from "../../../features/jobs/components/JobDetailNav";
 export default function Page() {
   const params = useParams<{ id: string }>();
   const jobPostingId = params?.id;
+  const { data: session } = useSession();
+  const joinType = session?.user?.join_type ?? "";
 
   return (
     <>
       <div>
-        <JobDetailNav />
+        <JobDetailNav join_type={joinType} />
         {jobPostingId && <JobDetailContent jobPostingId={jobPostingId} />}
       </div>
     </>
